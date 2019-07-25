692f7600efb86b109f23b55c9c03813de0005699
serg-sheridan <52678820+serg-sheridan@users.noreply.github.com>
2019-07-25 12:04:29 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,38 @@
+import { Song } from '../../app/mySongs-cons';
+
+
+export const SONG : Song[] = [
+    {
+        s_Name: 'Stairway to Heaven',
+        s_Artist: 'Led Zeppelin',
+        s_Genre: 'Rock and Roll',
+        s_Year: 1971,
+        //s_Pic: '../assets/images/led.jpg'
+        s_Pic: './assets/images/led.jpg',
+    },
+    {
+        s_Name: 'Highway to Hell',
+        s_Artist: 'ACDC',
+        s_Genre: 'Rock and Roll',
+        s_Year: 1979,
+        //s_Pic: '../assets/images/acdc.jpg',
+        s_Pic: './assets/images/acdc.jpg',
+    },
+    {
+        s_Name: 'Money',
+        s_Artist: 'Pink Floyd',
+        s_Genre: 'Rock and Roll',
+        s_Year: 1973,
+        //s_Pic: '../assets/images/pink.jpg',
+        s_Pic: './assets/images/pink.jpg',
+    },
+    {
+        s_Name: 'Purple Haze',
+        s_Artist: 'Jimi Hendrix',
+        s_Genre: 'Rock and Roll',
+        s_Year: 1970,
+        //s_Pic: './assets/images/jim.jpg',
+        s_Pic: './assets/images/jim.jpg',
+    }
+
+];
